f77839c76b1635c637985ef52c7be9b6a9d4f98c
themoffatt <adam@themoffatt.com>
2015-09-12 11:44:56 -0500

    small fixes

--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -1,12 +1,8 @@
-class VolunteerMailer < ActionMailer::Base
+class ClientMailer < ActionMailer::Base
   default from: 'referral@thesafehavennetwork.org'
 
   def confirm_signup(client)
     mail(to: client.email,  subject: 'signup confirmation')
-  end
-
-  def site_admin_email
-    'safe@thesafehavennetwork.org'
   end
 end
 
