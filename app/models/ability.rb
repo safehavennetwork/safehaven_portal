ecbb2d1bf756fd0726b28f9bb7899c1fd1c84a0e
themoffatt <adam@themoffatt.com>
2015-10-03 09:33:32 -0500

    formatting

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,32 +4,23 @@ class Ability
   attr_accessor :user
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
     @user = user
 
     can :update, User, user_id: user.id
 
     if user.site_admin?
-        can :manage, :all
+      can :manage, :all
     end
 
     if user.shelter?
-        can :update, Pet,          pet_id:          Pet.where(organization_id: org_id).pluck(:pet_id)
-        can :update, Organization, organization_id: org_id
+      can :update, Pet, pet_id: Pet.where(organization_id: org_id).pluck(:pet_id)
+      can :update, Organization, organization_id: org_id
     end
 
     if user.advocate?
-        can :update, Pet,          pet_id:          Client.includes(:pets).where(organization_id: org_id).pluck('pets.pet_id')
-        can :update, Client,       client_id:       Client.where(organization_id: org_id).pluck(:client_id)
-        can :update, Organization, organization_id: org_id
+      can :update, Pet, pet_id: Client.includes(:pets).where(organization_id: org_id).pluck('pets.pet_id')
+      can :update, Client, client_id: Client.where(organization_id: org_id).pluck(:client_id)
+      can :update, Organization, organization_id: org_id
     end
   end
 
