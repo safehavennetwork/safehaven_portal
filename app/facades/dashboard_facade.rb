f18bc257b9380265ee8bc68b0f2b7de591cb0f16
themoffatt <adam@themoffatt.com>
2015-10-03 00:18:26 -0500

    pending users highlighted and sorted to the top

--- app/facades/dashboard_facade.rb
+++ app/facades/dashboard_facade.rb
@@ -12,8 +12,7 @@ class DashboardFacade
 
   def admin_dashboard
     OpenStruct.new(defaults.merge({
-      pending_users:      User.pending,
-      all_users:          User.all.except{ |u| u.email == 'admin@safehaven.org'},
+      all_users:          User.where.not(email: 'admin@safehaven.org').order(:welcome_email_sent),
       all_clients:        Client.all,
       recent_activities:  GetRecentActivity.call,
       view:               'admin/dashboard',
