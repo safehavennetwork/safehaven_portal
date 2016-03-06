c42d3efe97cc523b507525f7ca235aa83f94de6c
Byron Weiss <byron.weiss@gmail.com>
2016-03-06 13:53:42 -0600

    Changed email provider to sendgrid for issue #112

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,12 +39,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'secure.thesafehavennetwork.org' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:               'smtp.gmail.com',
-    port:                  '587',
-    domain:                'gmail.com',
-    authentication:        'plain',
-    enable_startttls_auto: true,
-    user_name:             ENV['gmail_username'],
-    password:              ENV['gmail_password']
+    :user_name => ENV['sendgrid_username'],
+    :password => ENV['sendgrid_password'],
+    :domain => 'secure.thesafehavennetwork.org',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 end
